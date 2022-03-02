e654d4552084225f4e4243867009b131cfae466f
Ben Morris <64672316+SIRROMNEB@users.noreply.github.com>
2022-03-02 12:25:01 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,17 @@
 
 echo "Running setup script from GitHub..."
 echo "Installing required modules..."
-echo "1/7: moviepy"
+echo "1/7: 'moviepy'"
 sudo pip install moviepy 
-echo "2/7: pygame"
+echo "2/7: 'pygame'"
+sudo pip install pygame
+echo "3/7: 'screeninfo'"
+sudo pip install screeninfo
+echo "4/7: RPI.GPIO"
 echo "Skipping..."
+echo "5/7: 'subprocess'"
+sudo pip install subprocess
+echo "6/7: 'glob'"
+sudo pip install glob
+echo "7/7: 'os'"
+sudo pip install os
