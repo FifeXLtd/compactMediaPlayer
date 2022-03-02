342edabc850897fa36f026066f4877ec72dda65f
Ben Morris <64672316+SIRROMNEB@users.noreply.github.com>
2022-03-02 12:54:10 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,4 +17,15 @@ echo "Skipping..."
 echo "7/7: 'os'"
 echo "Skipping..."
 
+echo "Installing compactMediaPlayerScript"
 sudo wget -O /usr/local/bin/compactMediaPlayer.py https://raw.githubusercontent.com/FifeXLtd/compactMediaPlayer/main/compactMediaPlayer.py
+echo "Installing default thumbnail, video and audio files"
+echo "1/3: Thumbnail..."
+sudo wget -O /home/pi/thumbnail.jpg https://github.com/FifeXLtd/compactMediaPlayer/blob/dc1274a77cee455c1d60bc03b60c6e6dff0592b3/thumbnail.jpg
+echo "2/3: Video..."
+sudo wget -O /home/pi/video.mp4 https://github.com/FifeXLtd/compactMediaPlayer/blob/dc1274a77cee455c1d60bc03b60c6e6dff0592b3/video.mp4
+echo "3/3: Audio..."
+sudo wget -O /home/pi/audio.mp3 https://github.com/FifeXLtd/compactMediaPlayer/blob/dc1274a77cee455c1d60bc03b60c6e6dff0592b3/audio.mp3
+echo "Default files downloaded successfully to '/home/pi/'
+
+
