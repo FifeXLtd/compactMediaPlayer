be5b24a506d89161dbc021c69c3122cb118145c5
Ben Morris <64672316+SIRROMNEB@users.noreply.github.com>
2022-03-02 13:07:30 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+## Installation script for 'compactMediaPlayer' by Ben Morris for FifeX Ltd
+
+## First let's obtain the 'compactMediaPlayer.sh' script from GitHub 
+
+sudo wget -O /usr/local/bin/setup.sh https://raw.githubusercontent.com/FifeXLtd/compactMediaPlayer/main/setup.sh
+sudo chmod +x /usr/local/bin/setup.sh
+sudo /usr/local/bin/setup.sh
+
+echo "Installation successful"
