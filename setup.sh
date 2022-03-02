c728f9aaa0cd9cce23f674156f284666a7abbc39
Ben Morris <64672316+SIRROMNEB@users.noreply.github.com>
2022-03-02 12:20:27 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo "Success! we are reading from GitHub"
+echo "Running setup script from GitHub..."
+echo "Installing required modules..."
+echo "1/7: moviepy"
+sudo pip install moviepy 
+echo "2/7: pygame"
+echo "Skipping..."
