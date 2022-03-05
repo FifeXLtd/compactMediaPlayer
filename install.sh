7f91b78f4f391f11d0c3c3f0e4c0d1c79b845e99
Ben Morris <64672316+SIRROMNEB@users.noreply.github.com>
2022-03-05 19:47:58 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@
 
 ## First let's obtain the 'compactMediaPlayer.sh' script from GitHub 
 
-sudo wget -O /usr/local/bin/setup.sh https://raw.githubusercontent.com/FifeXLtd/compactMediaPlayer/main/setup.sh
-sudo chmod +x /usr/local/bin/setup.sh
-sudo /usr/local/bin/setup.sh
+sudo wget -O /home/pi/setup.sh https://raw.githubusercontent.com/FifeXLtd/compactMediaPlayer/main/setup.sh
+sudo chmod +x /home/pi/setup.sh
+sudo /home/pi/setup.sh
 
 echo "Installation successful"
 echo "Rebooting..."
