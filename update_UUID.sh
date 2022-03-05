1516d88de5fbe8f482fae89c1b6cf7af95665ab5
Ben Morris <64672316+SIRROMNEB@users.noreply.github.com>
2022-03-05 19:06:53 +0000

    Update update_UUID.sh

--- update_UUID.sh
+++ update_UUID.sh
@@ -2,18 +2,12 @@
 
 echo "Updating mount point with new storage device..."
 
-
-
 new_UUID=$(sudo blkid -s UUID -o value /dev/sda1)
 
 old_UUID=$(awk '{print $1}' /etc/fstab)
 old_UUID=$(echo $old_UUID| cut -c 11-19)
 
 lfstab_line=$(grep -n "rw" /etc/fstab)
-
-
-#echo "New listing"
-#echo $lfstab_line
 
 old_UUID="UUID=${old_UUID}"
 echo Old USB UUID: $old_UUID
@@ -26,7 +20,7 @@ b="/volume vfat defaults,auto,users,rw,nofail,noatime 0 0"
 new_line="${a} ${b}"
 echo "updated: ${new_line}"
 
-sudo sed -i '2 c '"$new_line"'/' /etc/fstab
+sudo sed -i '2 c '"$new_line"'' /etc/fstab
 
 echo "Remounting complete"
 echo "Rebooting system..."
